7665a84098745500d17c75fb57b9cd67a55bb537
Jeremy <jmarchand1@gmail.com>
2022-06-26 07:38:06 +0200

    Adding num remove test

--- tests/test_test.py
+++ tests/test_test.py
@@ -1,5 +1,7 @@
-def test1():
-    assert True
+from api.fast import num_remove
 
-def test2():
-    assert False
+def test_num_remove():
+    assert num_remove('F12') == 'F'
+
+# def test2():
+#     assert False
