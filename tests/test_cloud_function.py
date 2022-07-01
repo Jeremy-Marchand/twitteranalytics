0c40710866753654094bafb12af3326355ca2758
Jérémy <jmarchand1@gmail.com>
2022-07-01 16:02:06 +0200

    Adding get token test

--- tests/test_cloud_function.py
+++ tests/test_cloud_function.py
@@ -1,11 +1,24 @@
 import pandas as pd
-from cloud_function.main import last_date_db
+from cloud_function.main import last_date_db, get_token 
+
+def get_token_side_effect(token_name, default=None):
+    if token_name == 'BEARER_TOKEN':
+        return 'TOKEN_OK'
+    else:
+        return 'Key missing in env settings'
+
+def test_get_token(mocker):
+    mocker.patch('cloud_function.main.os.environ.get',
+                side_effect=get_token_side_effect)
+    assert get_token() == 'TOKEN_OK'
+
 
 def test_last_date_db(mocker):
-    df_test = pd.DataFrame({'created_at': ['2022-06-10 17:57:21 UTC']}, index = [0])
-    df_test['created_at'] = pd.to_datetime(df_test['created_at'])
+    df_test_last_date = pd.DataFrame({'created_at': ['2022-06-10 17:57:21 UTC']}, index = [0])
+    df_test_last_date['created_at'] = pd.to_datetime(df_test_last_date['created_at'])
     mocker.patch('cloud_function.main.pd.read_gbq',
-                return_value = df_test
+                return_value = df_test_last_date
                 )
-    date = last_date_db()
-    assert date == '2022-06-10T17:57:21Z'
+    assert last_date_db() == '2022-06-10T17:57:21Z'
+
+    
