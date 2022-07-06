072d5e46260bf7c92b4bc38f711e6fd469882b20
Jérémy <jmarchand1@gmail.com>
2022-07-06 17:02:33 +0200

    adding tests for CF

--- cloud_function/main.py
+++ cloud_function/main.py
@@ -40,8 +40,7 @@ def bearer_oauth(r):
     r.headers["User-Agent"] = "v2RecentSearchPython"
     return r
 
-
-def connect_to_endpoint(url, params):
+def connect_to_endpoint(url: str, params: str):
     response = requests.get(url, auth=bearer_oauth, params=params)
     print(response.status_code)
     if response.status_code != 200:
@@ -86,10 +85,10 @@ def main():
             most_recent_dt, response["meta"].get("next_token", False)
         )
     data = data.append(fetching_tweets(response), ignore_index=True)
-    table_id = "wagon-bootcamp-802.my_dataset.twitter_table"
-    data.to_gbq(table_id, if_exists="append")
-    print("Tweets successfully merged into the table")
-
+    table_id = 'wagon-bootcamp-802.my_dataset.twitter_table'
+    data.to_gbq(table_id, if_exists='append')
+    print('Tweets successfully merged into the table')
+    return None
 
 def twitter_update(event, context):
     """Triggered from a message on a Cloud Pub/Sub topic.
