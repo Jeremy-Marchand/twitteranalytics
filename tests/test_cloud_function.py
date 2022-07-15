51ff79e1ec06f60e985395dc8506a05b87cf51ba
Jeremy <jmarchand1@gmail.com>
2022-07-15 15:05:56 +0200

    Cleaning tests for last date

--- tests/test_cloud_function.py
+++ tests/test_cloud_function.py
@@ -16,15 +16,6 @@ def test_get_token(mocker):
                 side_effect=get_token_side_effect)
     assert get_token() == 'TOKEN_OK'
 
-
-def test_last_date_db(mocker):
-    df_test_last_date = pd.DataFrame({'created_at': ['2022-06-10 17:57:21 UTC']}, index = [0])
-    df_test_last_date['created_at'] = pd.to_datetime(df_test_last_date['created_at'])
-    mocker.patch('cloud_function.main.pd.read_gbq',
-                return_value = df_test_last_date
-                )
-    assert last_date_db() == '2022-06-10T17:57:21Z'
-
 class Tweet(TypedDict):
     lang: str
     created_at: str
