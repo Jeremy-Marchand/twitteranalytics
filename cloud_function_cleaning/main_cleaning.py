dd166f0f812dcbb6aa392e1fb9febc5d3e67ec8c
Jeremy <jmarchand1@gmail.com>
2022-07-21 11:58:02 +0200

    Testing CD with new folders added

--- cloud_function_cleaning/main_cleaning.py
+++ cloud_function_cleaning/main_cleaning.py
@@ -8,24 +8,9 @@ from cloud_function_cleaning.word_transformation import (
 )
 
 
-def drivers(date_start, date_end):
+def cleaning():
     # local test request : http://127.0.0.1:8000/drivers?date_start=2022-05-20%2009:00:00%2B00:00&date_end=2022-05-22%2009:00:00%2B00:00
-    query = """
-    SELECT *
-    FROM `wagon-bootcamp-802.my_dataset.twitter_table`
-    WHERE created_at > "{}" AND created_at < "{}"
-    ORDER BY created_at DESC
-    """
-    final_query = query.format(date_start, date_end)
-    project_id = "wagon-bootcamp-802"
-    # Uncomment to run in a container locally
-    #     credentials = service_account.Credentials.from_service_account_file(
-    #     'gcp_key/wagon-bootcamp-802-bd537eeb2bd3.json',
-    # )
-    # Uncomment final part of the following to run in a local container
-    df = pd.read_gbq(
-        final_query, project_id=project_id, dialect="standard"
-    )  # , credentials=credentials)
+    df = pd.DataFrame()
     df_clean = df.copy()
 
     # Removing RT mentions
@@ -45,12 +30,4 @@ def drivers(date_start, date_end):
     df_clean["text"] = df_clean["text"].str.replace(r" f ", " f1 ")
 
     # fetching final results using the drivers list
-    nb_tweets = {}
-    for driver, names in drivers_list.items():
-        mask = df_clean["text"].str.contains(f"{names[0]}", na=False)
-        if len(names) > 1:
-            for name in names[1:]:
-                mask = mask | df_clean["text"].str.contains(f"{name}", na=False)
-        nb_tweets[driver] = len(df_clean[mask])
-
-    return nb_tweets
+    return df_clean
